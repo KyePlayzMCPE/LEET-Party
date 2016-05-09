086221246228dc77491891ed52785e97db45b6ef
Professor Taki <Taki21@users.noreply.github.com>
2016-05-09 16:51:09 -0400

    Base Creation

---
+++ src/LeetParty/KyePlayzMCPE/Main.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LeetParty\KyPlayzMCPE;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+// use ....
+
+class Main extends PluginBase implements Listener {
+
+}
