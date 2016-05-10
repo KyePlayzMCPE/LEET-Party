f89181d724eba68190ae7937ec916167f1b3b593
jasonwynn10 <jasonwynn10@gmail.com>
2016-05-10 13:05:59 -0400

    Update Main.php
    A Good Base

--- src/LeetParty/KyePlayzMCPE/Main.php
+++ src/LeetParty/KyePlayzMCPE/Main.php
@@ -1,14 +1,31 @@
 <?php
-
-namespace LeetParty\KyPlayzMCPE;
+namespace LeetParty\KyePlayzMCPE;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
 use pocketmine\event\Listener;
-// use ....
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\Player;
 
 class Main extends PluginBase implements Listener {
-  
-  // public $property;
-  
-  // functions...
+  public $Cfg;
+  public function onEnable() {
+    $this->getCfg();
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->notice(TF::GREEN."Enabled!");
+  }
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+    if(strtolower($command) === "") {
+      //TODO
+    }
+    return false;
+  }
+  public function getCfg() {
+    $this->saveDefaultConfig();
+    $this->Cfg = $this->getConfig();
+  }
+  public function onDisable() {
+    $this->getLogger()->notice(TF::GREEN."Disabled!");
+  }
 }
